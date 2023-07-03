b3b03aca91d552fe38263e070ef0c07ce656ca64
Timo Heman <timoheman16@gmail.com>
2023-07-03 17:18:42 +0100

    added header element with navigation

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,11 @@
+import Header from "@/components/Header";
+
 function Home() {
-  return <div>Main Content</div>;
+	return (
+		<div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
+			<Header>header</Header>
+		</div>
+	);
 }
 
 export default Home;
---
+++ components/Header.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+interface HeaderProps {
+	children: React.ReactNode;
+	className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+	const router = useRouter();
+
+	const handleLogout = () => {
+		// handle logout
+	};
+
+	return (
+		<div
+			className={twMerge(
+				"h-fit bg-gradient-to-b from-emerald-800 p-6",
+				className
+			)}
+		>
+			<div className='w-full mb-4 flex items-center justify-between'>
+				<div className='hidden md:flex gap-x-2 items-center'>
+					<button
+						onClick={router.back}
+						className='rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'
+					>
+						<RxCaretLeft size={35} className='text-white' />
+					</button>
+					<button
+						onClick={router.forward}
+						className='rounded-full bg-black flex items-center justify-center hover:opacity-75 transition'
+					>
+						<RxCaretRight size={35} className='text-white' />
+					</button>
+				</div>
+				<div className='flex md:hidden gap-x-2 items-center'>
+					<button className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'>
+						<HiHome className='text-black' size={20} />
+					</button>
+					<button className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'>
+						<BiSearch className='text-black' size={20} />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
