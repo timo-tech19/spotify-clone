da02bcaee462112b72a47601ebcc4259232de22c
Timo Heman <timoheman16@gmail.com>
2023-07-04 09:29:43 +0100

    added supabase provide hoc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,27 @@
 import "./globals.css";
 import { Figtree } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Spotify Clone App",
-  description: "Listen to unlimited music all in one place",
+	title: "Spotify Clone App",
+	description: "Listen to unlimited music all in one place",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang='en'>
-      <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={font.className}>
+				<SupabaseProvider>
+					<Sidebar>{children}</Sidebar>
+				</SupabaseProvider>
+			</body>
+		</html>
+	);
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -34,8 +34,8 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 	);
 
 	return (
-		<aside className='flex h-full'>
-			<div className='hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2'>
+		<div className='flex h-full'>
+			<aside className='hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2'>
 				<Box>
 					<div className='flex flex-col gap-y-4 px-5 py-4'>
 						{routes.map((route) => (
@@ -46,9 +46,9 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 				<Box className='overflow-y-auto h-full'>
 					<Library />
 				</Box>
-			</div>
+			</aside>
 			<main className='overflow-y-auto h-full flex-1 py-2'>{children}</main>
-		</aside>
+		</div>
 	);
 };
 
