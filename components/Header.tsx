a094ec62b7abb4a8e6a4c1b6e3675a2366fe0e8a
Timo Heman <timoheman16@gmail.com>
2023-07-08 14:42:24 +0100

    add mobile routing

--- components/Header.tsx
+++ components/Header.tsx
@@ -61,10 +61,16 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
 					</button>
 				</div>
 				<div className='flex md:hidden gap-x-2 items-center'>
-					<button className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'>
+					<button
+						onClick={() => router.push("/")}
+						className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'
+					>
 						<HiHome className='text-black' size={20} />
 					</button>
-					<button className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'>
+					<button
+						onClick={() => router.push("/search")}
+						className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'
+					>
 						<BiSearch className='text-black' size={20} />
 					</button>
 				</div>
